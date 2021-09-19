a0c656c1c8108eb9bb5dc820dae3c8e3a46640e4
IsmailElShinnawy <ismailelshennawy@gmail.com>
2021-09-20 00:43:38 +0200

    done with about me section

--- src/home/components/AboutMe.jsx
+++ src/home/components/AboutMe.jsx
@@ -1,103 +1,103 @@
 import React from 'react';
 
 import selfImage from '../../assets/images/self-image.svg';
-import educationImage from '../../assets/images/education-image.svg';
-import skillsImage from '../../assets/images/skills-image.svg';
+// import educationImage from '../../assets/images/education-image.svg';
+// import skillsImage from '../../assets/images/skills-image.svg';
 
 import './AboutMe.scss';
 
-const sections = [
-  {
-    id: 'aboutMe',
-    title: 'About Me',
-    content: (
-      <div>
-        <p className='about-me-text'>
-          I am currently a software engineer 👨‍💻 at{' '}
-          <a
-            className='link'
-            href='https://www.mindeavors.com/'
-            target='_blank'
-            rel='noreferrer'
-          >
-            Mindeavors
-          </a>
-          , there I transform business ideas to software that people can make use of and
-          enjoy.
-        </p>
-        <p className='about-me-text'>
-          I am also a computer engineering 💻 junior @ the German University in Cairo,
-          studying computer engineering has been my dream since early years in my life and
-          I am really enjoying it.
-        </p>
-        <p className='about-me-text'>
-          I have been fencing 🤺 since 2012! I am have been a first-team member at Gezira
-          Sporting Club since 2016.
-        </p>
-      </div>
-    ),
-    imageSrc: selfImage,
-  },
-  {
-    id: 'education',
-    title: 'Education',
-    content: (
-      <div>
-        <ul>
-          <li className='mb-5'>
-            <h4 className='py-2'>
-              <u>Dar El Tarbiah Schools IGCSE</u>
-              <span className='years ms-2'>
-                <em>Sep 2015 - May 2018</em>
-              </span>
-            </h4>
-            <p>
-              Graduated with a perfect score of <strong>100%</strong>, scoring A* in all
-              12 subjects.
-            </p>
-            <p>
-              Awarded <strong>highest mark in Egypt</strong> for Advanced Subsidiary level
-              Physics for the May/June 2017 Cambridge IGCSE examinations
-            </p>
-          </li>
-          <li className='mb-5'>
-            <h4>
-              <u>German University in Cairo</u>
-              <span className='years ms-2'>
-                <em>Sep 2018 - May 2023</em>
-              </span>
-            </h4>
-            <p>
-              Media Engineering Technology faculty majoring in{' '}
-              <strong>Computer Science and Engineering</strong> with a GPA of{' '}
-              <strong>0.91 (A+)</strong>
-            </p>
-            <p>
-              Ranked <strong>5th</strong> in the Computer Science and Engineering group
-              (winter semester 2020/2021)
-            </p>
-            <p>
-              Ranked <strong>16th</strong> in the Engineering group (winter semester
-              2018/2019)
-            </p>
-          </li>
-        </ul>
-      </div>
-    ),
-    imageSrc: educationImage,
-  },
-  {
-    id: 'skills',
-    title: 'Skills',
-    content: <div></div>,
-    imageSrc: skillsImage,
-  },
-  {
-    id: 'contact',
-    title: 'Contact',
-    content: <div></div>,
-  },
-];
+// const sections = [
+//   {
+//     id: 'aboutMe',
+//     title: 'About Me',
+//     content: (
+//       <div>
+//         <p className='about-me-text'>
+//           I am currently a software engineer 👨‍💻 at{' '}
+//           <a
+//             className='link'
+//             href='https://www.mindeavors.com/'
+//             target='_blank'
+//             rel='noreferrer'
+//           >
+//             Mindeavors
+//           </a>
+//           , there I transform business ideas to software that people can make use of and
+//           enjoy.
+//         </p>
+//         <p className='about-me-text'>
+//           I am also a computer engineering 💻 junior @ the German University in Cairo,
+//           studying computer engineering has been my dream since early years in my life and
+//           I am really enjoying it.
+//         </p>
+//         <p className='about-me-text'>
+//           I have been fencing 🤺 since 2012! I am have been a first-team member at Gezira
+//           Sporting Club since 2016.
+//         </p>
+//       </div>
+//     ),
+//     imageSrc: selfImage,
+//   },
+//   {
+//     id: 'education',
+//     title: 'Education',
+//     content: (
+//       <div>
+//         <ul>
+//           <li className='mb-5'>
+//             <h4 className='py-2'>
+//               <u>Dar El Tarbiah Schools IGCSE</u>
+//               <span className='years ms-2'>
+//                 <em>Sep 2015 - May 2018</em>
+//               </span>
+//             </h4>
+//             <p>
+//               Graduated with a perfect score of <strong>100%</strong>, scoring A* in all
+//               12 subjects.
+//             </p>
+//             <p>
+//               Awarded <strong>highest mark in Egypt</strong> for Advanced Subsidiary level
+//               Physics for the May/June 2017 Cambridge IGCSE examinations
+//             </p>
+//           </li>
+//           <li className='mb-5'>
+//             <h4>
+//               <u>German University in Cairo</u>
+//               <span className='years ms-2'>
+//                 <em>Sep 2018 - May 2023</em>
+//               </span>
+//             </h4>
+//             <p>
+//               Media Engineering Technology faculty majoring in{' '}
+//               <strong>Computer Science and Engineering</strong> with a GPA of{' '}
+//               <strong>0.91 (A+)</strong>
+//             </p>
+//             <p>
+//               Ranked <strong>5th</strong> in the Computer Science and Engineering group
+//               (winter semester 2020/2021)
+//             </p>
+//             <p>
+//               Ranked <strong>16th</strong> in the Engineering group (winter semester
+//               2018/2019)
+//             </p>
+//           </li>
+//         </ul>
+//       </div>
+//     ),
+//     imageSrc: educationImage,
+//   },
+//   {
+//     id: 'skills',
+//     title: 'Skills',
+//     content: <div></div>,
+//     imageSrc: skillsImage,
+//   },
+//   {
+//     id: 'contact',
+//     title: 'Contact',
+//     content: <div></div>,
+//   },
+// ];
 
 const AboutMe = () => {
   // const [activeTab, setActiveTab] = useState('aboutMe');
@@ -113,23 +113,48 @@ const AboutMe = () => {
   //     </button>
   //   ))}
   // </div>
-  return sections.map((section, idx) => (
-    <section key={section.id} className=''>
-      <h2 className='display-5 title text-center pb-4'>{section.title}</h2>
-      <div className='row align-items-center'>
-        <div
-          className={`content col-12 col-md-8 ${
-            idx % 2 === 0 ? 'order-md-1' : 'order-md-2'
-          }`}
-        >
-          {section.content}
+  // return sections.map((section, idx) => (
+  //   <section key={section.id} className=''>
+  //     <h2 className='display-5 title text-center pb-4'>{section.title}</h2>
+  //     <div className='row align-items-center'>
+  //       <div
+  //         className={`content col-12 col-md-8 ${
+  //           idx % 2 === 0 ? 'order-md-1' : 'order-md-2'
+  //         }`}
+  //       >
+  //         {section.content}
+  //       </div>
+  //       <div className={`col-12 col-md-4 ${idx % 2 === 0 ? 'order-md-2' : 'order-md-1'}`}>
+  //         <img src={section.imageSrc} alt='illustration' />
+  //       </div>
+  //     </div>
+  //   </section>
+  // ));
+  return (
+    <div className='row about-me px-2 px-md-0'>
+      <div className='col-0 col-md-2' />
+      <div className='col-12 col-md-8 d-flex align-items-center shadow-lg main flex-column flex-lg-row py-2'>
+        <div className='px-4 py-4 py-lg-0'>
+          <h2 className='h2 title pb-3'>About me</h2>
+          <p className='about-me-text'>
+            I am currently a software engineer at{' '}
+            <a
+              className='link'
+              href='https://www.mindeavors.com/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Mindeavors
+            </a>
+            . Also, I am a computer engineering junior at the German University in Cairo.
+            A fencer for the first-team at Gezira Sporting Club.
+          </p>
         </div>
-        <div className={`col-12 col-md-4 ${idx % 2 === 0 ? 'order-md-2' : 'order-md-1'}`}>
-          <img src={section.imageSrc} alt='illustration' />
-        </div>
+        <img src={selfImage} alt='about-me illustration' />
       </div>
-    </section>
-  ));
+      <div className='col-0 col-md-2' />
+    </div>
+  );
 };
 
 export default AboutMe;
--- src/home/pages/Home.jsx
+++ src/home/pages/Home.jsx
@@ -9,43 +9,45 @@ import './Home.scss';
 
 const Home = () => {
   return (
-    <main className='container mt-5'>
-      <section className='about-me-section'>
-        <div className='row'>
-          <div className='col-12 col-md-7 order-2 order-md-1 text-center text-md-start d-block d-md-flex flex-column justify-content-center'>
-            <WhoAmI />
-            <div className='mt-3 social-links'>
-              <a
-                href='https://github.com/IsmailElShinnawy'
-                target='_blank'
-                rel='noreferrer'
-              >
-                <img src={githubIcon} alt='github' className='me-2' />
-              </a>
-              <a
-                href='https://linkedin.com/in/ismail-elshennawy/'
-                target='_blank'
-                rel='noreferrer'
-              >
-                <img src={linkedInIcon} alt='linkedIn' className='mx-2' />
-              </a>
-              <a
-                href='https://twitter.com/IsmailShinnawy'
-                target='_blank'
-                rel='noreferrer'
-              >
-                <img src={twitterIcon} alt='twitter' className='ms-2' />
-              </a>
+    <main className=''>
+      <div className='who-am-i-section'>
+        <section className='container'>
+          <div className='row'>
+            <div className='col-12 col-md-7 order-2 order-md-1 text-center text-md-start d-block d-md-flex flex-column justify-content-center'>
+              <WhoAmI />
+              <div className='mt-3 social-links'>
+                <a
+                  href='https://github.com/IsmailElShinnawy'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <img src={githubIcon} alt='github' className='me-2' />
+                </a>
+                <a
+                  href='https://linkedin.com/in/ismail-elshennawy/'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <img src={linkedInIcon} alt='linkedIn' className='mx-2' />
+                </a>
+                <a
+                  href='https://twitter.com/IsmailShinnawy'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <img src={twitterIcon} alt='twitter' className='ms-2' />
+                </a>
+              </div>
+            </div>
+            <div className='col-12 col-md-5 order-1 order-md-2 d-flex flex-column justify-content-center align-items-center mb-4 mb-md-0'>
+              <img src={myImage} alt='Ismail' className='my-image shadow' />
+              <div className='d-flex'></div>
             </div>
           </div>
-          <div className='col-12 col-md-5 order-1 order-md-2 d-flex flex-column justify-content-center align-items-center mb-4 mb-md-0'>
-            <img src={myImage} alt='Ismail' className='my-image shadow' />
-            <div className='d-flex'></div>
-          </div>
-        </div>
-        <div className='mt-5 about-me'>
-          <AboutMe />
-        </div>
+        </section>
+      </div>
+      <section className='about-me mt-4 container'>
+        <AboutMe />
       </section>
     </main>
   );
